ff53536b98c50b8278e2301a8e6772c1b1919544
Harrison Fung <harrison.hok.fung@gmail.com>
2020-08-05 07:29:26 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ class App extends Component {
 
   state = {
     data: {},
+    country: '',
   }
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({data: fetchedData});
-    console.log(fetchedData)
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country : country });
+  }
+
   render() {
     const { data } = this.state;
     
     return (
       <div className={styles.container}> 
       <Cards data = {data} />
-      <CountryPicker />
+      <CountryPicker handleCountryChange={this.handleCountryChange}/>
       <Chart />
       </div>
     );
